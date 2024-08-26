9d9460152ecc479a0f7ad063c24f09d59ec88470
Leonardo Lima <se.leonardolima@gmail.com>
2024-08-26 13:19:44 -0300

    Update theme colors

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,7 +1,15 @@
 export const theme = {
   colors: {
-    primary: "#ffffff",
-    secondary: "#000000",
+    black: "#000000",
+    blue500: '#457EFF',
+    blue700: '#145BFF',
+    blue900: "#141C2E",
+    gray100: "#F2F2F2",
+    gray300: "#AFB3BB",
+    green700: "#008000",
+    orange700: "#FFA500",
+    red500: "#FF4C4C",
+    red700: "#FF0000",
+    white: "#ffffff",
   }
 };
-
