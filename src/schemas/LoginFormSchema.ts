912b0b4975bd65fa2e972df9ad5139fdf8acdfe0
Leonardo Lima <se.leonardolima@gmail.com>
2024-08-26 16:42:42 -0300

    Update email validation error message

--- src/schemas/LoginFormSchema.ts
+++ src/schemas/LoginFormSchema.ts
@@ -1,6 +1,13 @@
 import * as yup from 'yup';
 
 export const schema = yup.object().shape({
-  email: yup.string().email().required('Email é obrigatório'),
+  email: yup.string().email('Email inválido').required('Email é obrigatório'),
   password: yup.string().required('Senha é obrigatória'),
 });
+
+export type FormData = yup.InferType<typeof schema>;
+
+// export type FormData = {
+//   email: string;
+//   password: string;
+// };
