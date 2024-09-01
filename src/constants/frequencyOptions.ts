2dea1318261bf66f77741f90089dbb03f4481f8c
Leonardo Lima <se.leonardolima@gmail.com>
2024-08-31 21:35:09 -0300

    Add frequency options for medication scheduling

---
+++ src/constants/frequencyOptions.ts
@@ -0,0 +1,5 @@
+export const frequencyOptions = [
+  { label: 'Diariamente', value: 'daily' },
+  { label: 'Duas vezes ao dia', value: 'twice_a_day' },
+  { label: 'Três vezes ao dia', value: 'three_times_a_day' },
+];
