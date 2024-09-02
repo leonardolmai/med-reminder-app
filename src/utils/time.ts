1240d0ac9da4673cfafa7f9d87aee856a909be37
Leonardo Lima <se.leonardolima@gmail.com>
2024-09-02 16:43:44 -0300

    Add getCurrentDate function

--- src/utils/time.ts
+++ src/utils/time.ts
@@ -2,3 +2,11 @@ export function formatTime(time: string) {
   const date = new Date(`2000-01-01T${time}`);
   return date.toLocaleTimeString([], { hour: 'numeric', minute: '2-digit' });
 }
+
+export function getCurrentDate(): string {
+  const today = new Date();
+  const month = (today.getMonth() + 1).toString().padStart(2, '0');
+  const day = today.getDate().toString().padStart(2, '0');
+  const date = `${day}/${month}/${today.getFullYear()}`;
+  return date;
+}
